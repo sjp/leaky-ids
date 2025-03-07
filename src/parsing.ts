5c839e20f6d474908dc0cf0d45774410ef92d994
Simon Potter <simon@sjp.co.nz>
2025-03-07 18:27:10 +1300

    Lint fix

--- src/parsing.ts
+++ src/parsing.ts
@@ -11,15 +11,16 @@ const IntegerParseFailure: ParseResult<number> = {
   result: null,
 };
 
-const ASCII_NUMERIC_CHARS_ONLY = /^\d+$/; 
+const ASCII_NUMERIC_CHARS_ONLY = /^\d+$/;
 
 export const parseIntegerId = (input: string): ParseResult<number> => {
   if (!input) {
     return IntegerParseFailure;
   }
 
-  if (!input.match(ASCII_NUMERIC_CHARS_ONLY))
+  if (!input.match(ASCII_NUMERIC_CHARS_ONLY)) {
     return IntegerParseFailure;
+  }
 
   const num = Number.parseInt(input, 10);
   if (!num) {
@@ -70,13 +71,13 @@ export const parseUlidId = (input: string): TimeBasedId | null => {
 // - 01956e96-c283-702e-9e6d-1e94c85ce6a6
 // - 01956e96c283702e9e6d1e94c85ce6a6
 const HYPHEN_STRIPPED_UUID = /^(.{8})(.{4})(.{4})(.{4})(.{12})$/;
-const HYPHEN_STRIPPED_UUID_LENGTH = 32; // e.g. 
+const HYPHEN_STRIPPED_UUID_LENGTH = 32; // e.g.
 
 const normalizeUuid = (input: string): string => {
-  return input.length === HYPHEN_STRIPPED_UUID_LENGTH 
-    ? input.replace(HYPHEN_STRIPPED_UUID, "$1-$2-$3-$4-$5") 
+  return input.length === HYPHEN_STRIPPED_UUID_LENGTH
+    ? input.replace(HYPHEN_STRIPPED_UUID, "$1-$2-$3-$4-$5")
     : input;
-}
+};
 
 const getTimestampFromUuidV7 = (input: string): number | null => {
   if (!uuidValidate(input)) {

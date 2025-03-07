4874e893f78ae538dce20be5992f5561907ea2a7
Simon Potter <simon@sjp.co.nz>
2025-03-07 18:35:30 +1300

    Enable the table to occupy full-width.
    Means that the labels are not cramped next to the dates

--- src/timestamp.tsx
+++ src/timestamp.tsx
@@ -10,7 +10,7 @@ export const Timestamp = ({ timestamp }: TimestampProps) => {
   const tz = getTimeZoneName();
 
   return (
-    <table>
+    <table width="100%">
       <tbody style={{ textAlign: "left" }}>
         <tr>
           <th scope="row">Creation Date</th>

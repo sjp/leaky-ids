f311ca920175ec481d0619df3849b77c80b4e74e
Simon Potter <simon@sjp.co.nz>
2026-06-03 13:16:57 +1000

    Improve table styling

--- src/timestamp.tsx
+++ src/timestamp.tsx
@@ -10,7 +10,7 @@ export const Timestamp = ({ timestamp }: TimestampProps) => {
   const tz = getTimeZoneName();
 
   return (
-    <table width="100%">
+    <table style={{ width: "100%" }}>
       <tbody style={{ textAlign: "left" }}>
         <tr>
           <th scope="row">Creation Date</th>
